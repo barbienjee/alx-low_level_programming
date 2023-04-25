873fa5351a83c297a36cb373f8af0a53d0c26532
barbienjee <bngonyo97@gmail.com>
2023-04-24 17:01:47 -0700

    Print  different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: 0
+  */
+int main(void)
+{
+	int n, m, p;
+
+	for (n = '0'; n < '9'; n++)
+	{
+	for (m = n + 1; m <= '9'; m++)
+	{
+	for (p = m + 1; p <= '9'; p++)
+	{
+		if ((m != n) != p)
+		{
+		putchar(n);
+		putchar(m);
+		putchar(p);
+		if (n == '7' && m == '8')
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
