aa97181dead7a75b8bb827448bf2c165446f9b20
barbienjee <bngonyo97@gmail.com>
2023-04-29 10:09:30 -0700

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		_putchar('-');
+		_putchar(45);
+		k = -k;
 	}
-	k /= 10;
-	if (k != 0)
+	if (k / 10)
 	{
-		print_number(k);
-		_putchar((unsigned int) n % 10 + '0');
+		print_number(k / 10);
 	}
-	}
+	_putchar(k % 10 + '0');
+}
