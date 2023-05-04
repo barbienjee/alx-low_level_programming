d9b257a8231df7358aea328f8bf7b3c23307b652
barbienjee <bngonyo97@gmail.com>
2023-05-04 14:32:18 -0700

    function that prints an integer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
  *leet - encodes a string into 1337
- *@str: The string to be encoded
- *Return: A pointer to the encoded string
+ *Letters a and A should be replaced by 4
+ *Letters e and E should be replaced by 3
+ *Letters o and O should be replaced by 0
+ *Letters t and T should be replaced by 7
+ *Letters l and L should be replaced by 1
+ *@s: Pointer to string
+ *Return: A pointer to s
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'o', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int string_len, leetcount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711"
 
 	while (str[indx1])
 	{
