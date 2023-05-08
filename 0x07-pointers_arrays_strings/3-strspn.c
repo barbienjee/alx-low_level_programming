00669dee4bf4627c70d27326f666ed76119d7700
barbienjee <bngonyo97@gmail.com>
2023-05-08 13:33:19 -0700

    function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s: input
+ *@accept: input
+ *Return: Number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int 
